9ba11a769482d5698aa144404d8dbdb06fd32faf
Ian Chang <ywc261@nyu.edu>
2025-04-18 15:53:14 +0900

    edited df path

--- qc-request/qc_form_generator.py
+++ qc-request/qc_form_generator.py
@@ -6,7 +6,7 @@ from collections import defaultdict
 # Run with: streamlit run qc-request/qc_form_generator.py
 
 # Load test case dataset
-df = pd.read_csv("data/processed/processed_KOCOWA_4.0_tc_connectedTV.csv")
+df = pd.read_csv("qc-request/data/processed/processed_KOCOWA_4.0_tc_connectedTV.csv")
 
 # Define available devices
 available_devices = ['Android Mobile', 'Apple Mobile', 'Android TV', 'Apple TV', 'Fire TV', 'Roku', 'Web', 'Smart TV', 'Vizio TV']
