91c98c3c02a481e407176836889a2de784fad266
offthemenu <ywc261@nyu.edu>
2025-07-09 15:52:03 +0900

    added connected tv cases

--- qc-request/plan-page-processing.py
+++ qc-request/plan-page-processing.py
@@ -6,7 +6,7 @@ today = datetime.today().replace(hour=0, minute=0, second=0, microsecond=0).strf
 df_preProject = pd.read_csv("processed-data/processed_KOCOWA_4.0_tc_final.csv")
 
 plan_page_object = [
-    {'project_name': 'Plan Page Improvement', 'main_category': 'Subscribe', 'scope_of_dev': None, 'Web': True},
+    {'project_name': 'Plan Page Improvement', 'main_category': 'Subscribe', 'scope_of_dev': "Subscribe", 'Web': True},
     {'project_name': 'Plan Page Improvement', 'main_category': 'Subscribe', 'scope_of_dev': 'Hover', 'Web': True},
     {'project_name': 'Plan Page Improvement', 'main_category': 'Subscribe', 'scope_of_dev': 'Subscribed', 'Web': True},
     {'project_name': 'Plan Page Improvement', 'main_category': 'Subscribe', 'scope_of_dev': 'Upgrade > Basic Monthly', 'Web': True},
@@ -38,20 +38,42 @@ plan_page_object = [
     {'project_name': 'Plan Page Improvement','Android Mobile': True, 'main_category': 'Subscribe', 'scope_of_dev': 'Promotion > Counthdown > Time (Pad)'},
     {'project_name': 'Plan Page Improvement','Android Mobile': True, 'main_category': 'Subscribe', 'scope_of_dev': 'Promotion > Premiun (Mobile)'},
     {'project_name': 'Plan Page Improvement','Android Mobile': True, 'main_category': 'Subscribe', 'scope_of_dev': 'Promotion > Basic > Countdown > Day (Mobile)'},
-
+    {'project_name': 'Plan Page Improvement','Android TV': True, 'main_category': 'Subscribe', 'scope_of_dev': 'Subscribe'},
+    {'project_name': 'Plan Page Improvement','Android TV': True, 'main_category': 'Subscribe', 'scope_of_dev': 'Subscribed'},
+    {'project_name': 'Plan Page Improvement','Android TV': True, 'main_category': 'Subscribe', 'scope_of_dev': 'Restore Purchase > Focused'},
+    {'project_name': 'Plan Page Improvement','Android TV': True, 'main_category': 'Subscribe', 'scope_of_dev': 'Promotion'},
+    {'project_name': 'Plan Page Improvement','Android TV': True, 'main_category': 'Subscribe', 'scope_of_dev': 'Promotion > Subscribed'},
+    {'project_name': 'Plan Page Improvement','Fire TV': True, 'main_category': 'Subscribe', 'scope_of_dev': 'Subscribe'},
+    {'project_name': 'Plan Page Improvement','Fire TV': True, 'main_category': 'Subscribe', 'scope_of_dev': 'Subscribed'},
+    {'project_name': 'Plan Page Improvement','Fire TV': True, 'main_category': 'Subscribe', 'scope_of_dev': 'Restore Purchase > Focused'},
+    {'project_name': 'Plan Page Improvement','Fire TV': True, 'main_category': 'Subscribe', 'scope_of_dev': 'Promotion'},
+    {'project_name': 'Plan Page Improvement','Fire TV': True, 'main_category': 'Subscribe', 'scope_of_dev': 'Promotion > Subscribed'},
+    {'project_name': 'Plan Page Improvement','Apple TV': True, 'main_category': 'Subscribe', 'scope_of_dev': 'Subscribe'},
+    {'project_name': 'Plan Page Improvement','Apple TV': True, 'main_category': 'Subscribe', 'scope_of_dev': 'Subscribed'},
+    {'project_name': 'Plan Page Improvement','Apple TV': True, 'main_category': 'Subscribe', 'scope_of_dev': 'Restore Purchase > Focused'},
+    {'project_name': 'Plan Page Improvement','Apple TV': True, 'main_category': 'Subscribe', 'scope_of_dev': 'Promotion'},
+    {'project_name': 'Plan Page Improvement','Apple TV': True, 'main_category': 'Subscribe', 'scope_of_dev': 'Promotion > Subscribed'},
+    {'project_name': 'Plan Page Improvement','Smart TV': True, 'main_category': 'Subscribe', 'scope_of_dev': 'Subscribe'},
+    {'project_name': 'Plan Page Improvement','Smart TV': True, 'main_category': 'Subscribe', 'scope_of_dev': 'Subscribed'},
+    {'project_name': 'Plan Page Improvement','Smart TV': True, 'main_category': 'Subscribe', 'scope_of_dev': 'Promotion'},
+    {'project_name': 'Plan Page Improvement','Smart TV': True, 'main_category': 'Subscribe', 'scope_of_dev': 'Promotion > Subscribed'},
+    {'project_name': 'Plan Page Improvement','Smart TV': True, 'main_category': 'Subscribe', 'scope_of_dev': 'BR'},
+    {'project_name': 'Plan Page Improvement','Smart TV': True, 'main_category': 'Subscribe', 'scope_of_dev': 'BR > Scroll 1'},
+    {'project_name': 'Plan Page Improvement','Smart TV': True, 'main_category': 'Subscribe', 'scope_of_dev': 'BR > Scroll 2'},
+    
 ]
 
-df_planPage = pd.DataFrame(plan_page_object)
+df_planPage = pd.DataFrame(plan_page_object).fillna(False).reset_index(drop=True)
+df_planPage['test_case']=None
 
-columns = ['project_name', 'main_category', 'scope_of_dev', 'test_case', 'Fire TV','Roku', 'Android TV', 'Apple TV', 'Web', 'Apple Mobile', 'Android Mobile', 'Smart TV', 'Vizio TV']
-df_planPage['test_case']=False
-df_planPage['Fire TV']=False
-df_planPage['Roku']=False
-df_planPage['Android TV']=False
-df_planPage['Apple TV']=False
-df_planPage['Smart TV']=False
-df_planPage['Vizio TV']=False
+# columns = ['project_name', 'main_category', 'scope_of_dev', 'test_case', 'Fire TV','Roku', 'Android TV', 'Apple TV', 'Web', 'Apple Mobile', 'Android Mobile', 'Smart TV', 'Vizio TV']
+# df_planPage['Fire TV']=False
+# df_planPage['Roku']=False
+# df_planPage['Android TV']=False
+# df_planPage['Apple TV']=False
+# df_planPage['Smart TV']=False
+# df_planPage['Vizio TV']=False
 
 df_project = pd.concat([df_preProject, df_planPage], ignore_index=True)
 
-df_project.to_csv("processed-data/combined_project_test_cases.csv")
+df_project.to_csv("processed-data/combined_project_test_cases.csv", index=False)
